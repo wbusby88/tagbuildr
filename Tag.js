8cb909b4314f840a6258b727133f745b098be131
Will Busby <will0609@hotmail.com>
2017-02-23 15:30:44 +0000

    Added Tag.js

---
+++ Tag.js
@@ -0,0 +1,134 @@
+/**
+ * Tag.js - Utility to create a DOM element and add chilren elements using a declarative string syntax
+ * @author Will Busby - Haymarket Media
+ * 
+ */
+(function (undefined) {
+    'use strict';
+
+    var isElement = void 0;
+    if ("HTMLElement" in window) {
+        isElement = function isElement(obj) {
+            return obj instanceof HTMLElement;
+        };
+    } else {
+        isElement = function isElement(obj) {
+            return !!(obj && typeof obj === "object" && obj.nodeType === 1 && obj.nodeName);
+        };
+    }
+
+    var isArray = void 0;
+
+    if (Array.isArray) {
+        isArray = Array.isArray;
+    } else {
+        isArray = $.isArray;
+    }
+
+    /**
+        * You can have multiple nested calls to the create function from within the children arrays
+        * @param {String} tagString The element tag string. Use a declarative syntax to define the tag and attributes e.g. h1.title|data-attr=foo, div#main-div etc
+        * @param {Dynamic} children Either a single child element|string|number or A mixed array of strings|numbers|elements to add as children
+        * @return {Element} full rendered DOM element
+    */
+    window.Tag = function tagBuilder(tagString, children) {
+        //transform class and id strings to compatible attributes
+        tagString = _handleClassesAndId(tagString);
+        var tagStrArray = tagString.split(/\|/);
+        var tag = tagStrArray.shift();
+        var el = document.createElement(tag);
+        var attrs = _sortAttrs(tagStrArray);
+
+        _setDomAttrs(attrs, el);
+        //finished with attrs
+        attrs = null;
+
+        //return now if no children
+        if (!children) {
+            return el;
+        }
+
+        //if not an array, assume single child and append
+        if (!isArray(children)) {
+            _append(children, el);
+            return el;
+        } else {
+            /**
+            * Children array can have a mix of strings and javascript DOM elements. It looks for the string and uses the more optimised insertAdjacentHTML or simple appends the child to the element.
+            */
+            _appendChildren(children, el);
+        }
+        //finished with children
+        children = null;
+
+        return el;
+    };
+
+    var attrType = {
+        '.': 'className',
+        '#': 'id'
+    };
+
+    function _handleClassesAndId(str) {
+        return str.replace(/(.*?)([\.|#].[^|]*)(.*)/, function (fullStr, tag, classAndIds, end) {
+            if (classAndIds[0] === '|') {
+                return fullStr;
+            }
+            var clsIdArr = classAndIds.split(/(\..[^\.|#]*)/).filter(Boolean);
+            var returnArr = [];
+            var i = clsIdArr.length;
+            while (i--) {
+                returnArr.push("|" + attrType[clsIdArr[i][0]] + "=" + clsIdArr[i].substr(1));
+            }
+            clsIdArr = null;
+            return "" + tag + returnArr.join('') + end;
+        });
+    }
+
+    function _sortAttrs(arr) {
+        var attrs = {};
+        for (var i = 0, l = arr.length; i < l; i++) {
+            if (/=/.test(arr[i])) {
+                var firstEquals = arr[i].indexOf('=');
+                var attr = arr[i].substr(0, firstEquals);
+                var value = arr[i].substr(firstEquals + 1);
+                var current = attrs[attr];
+                if (current) {
+                    current += " " + value;
+                } else {
+                    current = value;
+                }
+                attrs[attr] = current;
+            }
+        }
+        return attrs;
+    }
+
+    function _setDomAttrs(attrs, el) {
+        var attr = void 0;
+        for (attr in attrs) {
+            if (attr === 'className' || attr === 'id') {
+                el[attr] = attrs[attr];
+            } else if (attrs.hasOwnProperty(attr)) {
+                el.setAttribute(attr, attrs[attr]);
+            }
+        }
+    }
+
+    function _appendChildren(children, el) {
+        var i = 0;
+        var len = children.length;
+        for (; i < len; i++) {
+            _append(children[i], el);
+        }
+    }
+
+    function _append(child, el) {
+        if (isElement(child)) {
+            el.appendChild(child);
+        } else {
+            el.insertAdjacentHTML('beforeend', child);
+        }
+    }
+
+})();
